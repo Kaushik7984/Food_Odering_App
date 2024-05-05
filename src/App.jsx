e17bdaea5bde4dc3a5d8b59eb1dd2bdab70fc1e5
Kaushik7984 <kaushik7984@gmail.com>
2024-05-06 04:05:54 +0530

    adding all file

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Components/Header";
+import Meals from "./Components/Meals";
+import { CartContextProvider } from "./Store/cartContext"; 
+
+function App() {
+  return (
+    
+      <CartContextProvider>
+        <Header />
+        <Meals />
+      </CartContextProvider>
+    
+  );
+}
+
+export default App;
